56c9950b49dae1ec11bab6393036453c76001243
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-11 22:52:00 +0800

    >  compile NEMU 2020216001 lzkctrl Linux lzkctrl-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:52:00 up  8:53,  1 user,  load average: 0.08, 0.03, 0.01 e2c3d098e452de27b32a1c98c8caab5e1d5e8a9

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -5,23 +5,12 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE_S dest = op_dest->val;
-	DATA_TYPE_S result = op_dest->val;
+
 	uint8_t count = src & 0x1f;
 	dest >>= count;
-	result = dest;
-	int len = (DATA_BYTE << 3) - 1;
-	cpu.eflags.CF=0;
-	cpu.eflags.OF=0;
-	cpu.eflags.SF=result >> len;
-    	cpu.eflags.ZF=!result;
-    	OPERAND_W(op_dest, dest);
-	result ^= result >>4;
-	result ^= result >>2;
-	result ^= result >>1;
-	cpu.eflags.PF=!(result & 1);
-	/* There is no need to update EFLAGS, since no other instructions 
-	 * in PA will test the flags updated by this instruction.
-	 */
+	OPERAND_W(op_dest, dest);
+
+	update_eflags_pf_zf_sf(dest);
 
 	print_asm_template2();
 }
