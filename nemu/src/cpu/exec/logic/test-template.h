5c89b3cd2c0636fc768a948b3cf240f8278b127e
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-11 23:30:04 +0800

    >  compile NEMU 2020216001 lzkctrl Linux lzkctrl-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:30:04 up  9:31,  1 user,  load average: 0.02, 0.05, 0.24 b9613c794ebee116d7082e859ffa1749a1afd7e4

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -2,27 +2,16 @@
 
 #define instr test
 
-static void do_execute()
-{
-    DATA_TYPE r=op_dest->val&op_src->val;
-	int len=(DATA_BYTE<<3)-1;
- 	cpu.eflags.OF=0;
-	cpu.eflags.CF=0;
-	cpu.eflags.SF=r>>len;
-	cpu.eflags.ZF=!r;
-	r^=r>>4;
-	r^=r>>2;
-	r^=r>>1;
-	cpu.eflags.PF=!(r&1);
+static void do_execute () {
+	DATA_TYPE result = op_dest->val & op_src->val;
+
+	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = cpu.eflags.OF = 0;
+
 	print_asm_template2();
-    /**/
 }
 
-make_instr_helper(i2a)
 make_instr_helper(i2rm)
-#if DATA_BYTE==2 || DATA_BYTE==4
-make_instr_helper(si2rm)
-#endif
 make_instr_helper(r2rm)
 
 #include "cpu/exec/template-end.h"
