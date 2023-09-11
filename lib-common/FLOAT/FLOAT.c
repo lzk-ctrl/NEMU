6a1d57dcfb78e7a687d081177aa69b6dff63b620
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-11 23:22:39 +0800

    >  compile NEMU 2020216001 lzkctrl Linux lzkctrl-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:22:39 up  9:23,  1 user,  load average: 0.07, 0.10, 0.37 60d1040cff44d5aba488337939445fd85880d8d9

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,44 +1,17 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	long long c=(long long )a*(long long)b;
-	return (FLOAT)(c>>16);
+	long long c = (long long)a * (long long)b;
+	return (FLOAT)(c >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	/* Dividing two 64-bit integers needs the support of another library
-	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-	 * here. A "64/32" division is enough.
-	 *
-	 * To perform a "64/32" division, you can use the x86 instruction
-	 * `div' or `idiv' by inline assembly. We provide a template for you
-	 * to prevent you from uncessary details.
-	 *
-	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-	 *
-	 * If you want to use the template above, you should fill the "???"
-	 * correctly. For more information, please read the i386 manual for
-	 * division instructions, and search the Internet about "inline assembly".
-	 * It is OK not to use the template above, but you should figure
-	 * out another way to perform the division.
-	 */
 	FLOAT p, q;
 	asm volatile("idiv %2" : "=a"(p), "=d"(q) : "r"(b), "a"(a << 16), "d"(a >> 16));
 	return p;
 }
 
 FLOAT f2F(float a) {
-	/* You should figure out how to convert `a' into FLOAT without
-	 * introducing x87 floating point instructions. Else you can
-	 * not run this code in NEMU before implementing x87 floating
-	 * point instructions, which is contrary to our expectation.
-	 *
-	 * Hint: The bit representation of `a' is already on the
-	 * stack. How do you retrieve it to another variable without
-	 * performing arithmetic operations on it directly?
-	 */
-
 	int b = *(int *)&a;
 	int sign = b >> 31;
 	int exp = (b >> 23) & 0xff;
@@ -57,8 +30,17 @@ FLOAT f2F(float a) {
 }
 
 FLOAT Fabs(FLOAT a) {
-	return (a>0)?a:-a;
+	FLOAT b;
+	if (a > 0){
+		b = a;
+	} else {
+		b = -a;
+
+	}
+	return b;
 }
+// ...
+
 
 /* Functions below are already implemented */
 
