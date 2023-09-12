845e9acb05cce3cb7568259b750d673d6c5854bd
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-12 17:38:01 +0800

    >  compile NEMU 3021005049 lzkctrl Linux lzkctrl-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:38:01 up 14:39,  1 user,  load average: 0.52, 2.36, 1.42 b9eae92540469ef95f6807e11dfc6107bee02e2

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -6,81 +6,40 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	/* Dividing two 64-bit integers needs the support of another library
-	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-	 * here. A "64/32" division is enough.
-	 *
-	 * To perform a "64/32" division, you can use the x86 instruction
-	 * `div' or `idiv' by inline assembly. We provide a template for you
-	 * to prevent you from uncessary details.
-	 *
-	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-	 *
-	 * If you want to use the template above, you should fill the "???"
-	 * correctly. For more information, please read the i386 manual for
-	 * division instructions, and search the Internet about "inline assembly".
-	 * It is OK not to use the template above, but you should figure
-	 * out another way to perform the division.
-	 */
-
-	int sign = 1;
-	if (a < 0) 
-	{
-		sign = -sign;
-		a = -a;
-	}
-	if (b < 0) 
-	{
-		sign = -sign;
-		b = -b;
-	}
-	int res = a / b;
-	a = a % b;
-	int i;
-	for (i = 0; i < 16; i++) 
-	{
-		a <<= 1;
-		res <<= 1;
-		if (a >= b) 
-		{
-			a -= b;
-			res++;
-		}
-	}
-	return res * sign;
+	FLOAT p, q;
+	asm volatile("idiv %2" : "=a"(p), "=d"(q) : "r"(b), "a"(a << 16), "d"(a >> 16));
+	return p;
 }
 
 FLOAT f2F(float a) {
-	/* You should figure out how to convert `a' into FLOAT without
-	 * introducing x87 floating point instructions. Else you can
-	 * not run this code in NEMU before implementing x87 floating
-	 * point instructions, which is contrary to our expectation.
-	 *
-	 * Hint: The bit representation of `a' is already on the
-	 * stack. How do you retrieve it to another variable without
-	 * performing arithmetic operations on it directly?
-	 */
-
 	int b = *(int *)&a;
 	int sign = b >> 31;
 	int exp = (b >> 23) & 0xff;
-	FLOAT k = b & 0x7fffff;
-	if (exp != 0) k += 1 << 23;
+	FLOAT c = b & 0x7fffff;
+	if (exp != 0) {
+		c += 1 << 23;
+	}
 	exp -= 150;
-	if (exp < -16) k >>= -16 - exp;
-	if (exp > -16) k <<= exp + 16;
-	return sign == 0 ? k : -k;
+	if (exp < -16) {
+		c >>= -16 - exp;
+	}
+	if (exp > -16) {
+		c <<= exp + 16;
+	}
+	return sign == 0 ? c : -c;
 }
 
 FLOAT Fabs(FLOAT a) {
 	FLOAT b;
-	if (a < 0)
-		b = - a;
-	else
+	if (a > 0){
 		b = a;
+	} else {
+		b = -a;
+
+	}
 	return b;
 }
+// ...
 
 /* Functions below are already implemented */
 
