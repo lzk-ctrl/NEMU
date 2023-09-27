8418f219d7b1e02f35458551113019b8318379f0
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-27 21:55:48 +0800

    >  compile NEMU 2020216001 lzkctrl Linux lzkctrl-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:55:48 up 3 min,  1 user,  load average: 0.25, 0.44, 0.21 355c41a70e68a70f51c9a05fe0f327237435623

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,52 +1,45 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	long long c = (long long)a * (long long)b;
+	return (FLOAT)(c >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	/* Dividing two 64-bit integers needs the support of another library
-	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-	 * here. A "64/32" division is enough.
-	 *
-	 * To perform a "64/32" division, you can use the x86 instruction
-	 * `div' or `idiv' by inline assembly. We provide a template for you
-	 * to prevent you from uncessary details.
-	 *
-	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-	 *
-	 * If you want to use the template above, you should fill the "???"
-	 * correctly. For more information, please read the i386 manual for
-	 * division instructions, and search the Internet about "inline assembly".
-	 * It is OK not to use the template above, but you should figure
-	 * out another way to perform the division.
-	 */
-
-	nemu_assert(0);
-	return 0;
+	FLOAT p, q;
+	asm volatile("idiv %2" : "=a"(p), "=d"(q) : "r"(b), "a"(a << 16), "d"(a >> 16));
+	return p;
 }
 
 FLOAT f2F(float a) {
-	/* You should figure out how to convert `a' into FLOAT without
-	 * introducing x87 floating point instructions. Else you can
-	 * not run this code in NEMU before implementing x87 floating
-	 * point instructions, which is contrary to our expectation.
-	 *
-	 * Hint: The bit representation of `a' is already on the
-	 * stack. How do you retrieve it to another variable without
-	 * performing arithmetic operations on it directly?
-	 */
-
-	nemu_assert(0);
-	return 0;
+	int b = *(int *)&a;
+	int sign = b >> 31;
+	int exp = (b >> 23) & 0xff;
+	FLOAT c = b & 0x7fffff;
+	if (exp != 0) {
+		c += 1 << 23;
+	}
+	exp -= 150;
+	if (exp < -16) {
+		c >>= -16 - exp;
+	}
+	if (exp > -16) {
+		c <<= exp + 16;
+	}
+	return sign == 0 ? c : -c;
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	FLOAT b;
+	if (a > 0){
+		b = a;
+	} else {
+		b = -a;
+
+	}
+	return b;
 }
+// ...
 
 /* Functions below are already implemented */
 
@@ -73,4 +66,3 @@ FLOAT pow(FLOAT x, FLOAT y) {
 
 	return t;
 }
-
