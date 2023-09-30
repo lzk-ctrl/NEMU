859ae096a2d73cfbc25d07266008089bb64bfbf4
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-30 11:04:18 +0800

    >  compile NEMU 3021005049 lzkctrl Linux lzkctrl-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:04:18 up  2:30,  1 user,  load average: 1.01, 0.58, 0.35 5bba53a18b84154ea72d1ad69b3587619a338811

--- nemu/src/cpu/exec/arith/sub.c
+++ nemu/src/cpu/exec/arith/sub.c
@@ -1,6 +1,5 @@
 #include "cpu/exec/helper.h"
 
-
 #define DATA_BYTE 2
 #include "sub-template.h"
 #undef DATA_BYTE
@@ -12,6 +11,7 @@
 /* for instruction encoding overloading */
 
 make_helper_v(sub_i2rm)
-make_helper_v(sub_si2rm)
-make_helper_v(sub_r2rm)
-make_helper_v(sub_rm2r)
+    make_helper_v(sub_si2rm)
+        make_helper_v(sub_r2rm)
+            make_helper_v(sub_rm2r)
+                make_helper_v(sub_i2a)
